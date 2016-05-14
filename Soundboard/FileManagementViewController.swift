15eb172590f1c6a8571c602bebcca8bb1f37471e
Jan Hindermann <jan@dfragment.net>
2016-05-14 15:31:22 +0200

    File management view can list and delete music files

--- Soundboard/FileManagementViewController.swift
+++ Soundboard/FileManagementViewController.swift
@@ -10,13 +10,43 @@ import UIKit
 
 class FileManagementViewController: UITableViewController {
 
+    var theFiles : [NSURL]?
+    let permittedExtensions : [String] = ["mp3", "m4a", "mp4", "wav"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadFiles()
         self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func loadFiles(updateTable: Bool = false) {
+        let documentFolder =  NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+        
+        do {
+            let allFiles = try NSFileManager.defaultManager().contentsOfDirectoryAtURL(documentFolder, includingPropertiesForKeys: nil, options: NSDirectoryEnumerationOptions())
+            
+            // filter result set: only MP3/M4A/AAC/WAV files
+            let mappedFiles = allFiles.filter({ (fileUrl) -> Bool in
+                return permittedExtensions.contains(fileUrl.pathExtension?.lowercaseString ?? "")
+            })
+            
+            if mappedFiles.isEmpty {
+                showError("Could not find any music files.", title: "No music files")
+            } else {
+                theFiles = mappedFiles
+            }
+        } catch let error as NSError {
+            print("Error while enumerating files", error)
+            showError("Could not load sound files: \(error)", title: "Error")
+        }
+        
+        if updateTable {
+            tableView.reloadData()
+        }
     }
 
     // MARK: - Table view data source
@@ -26,26 +56,34 @@ class FileManagementViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return theFiles?.count ?? 0
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("soundfileCell", forIndexPath: indexPath)
 
-        // Configure the cell...
+        let file = theFiles![indexPath.row]
+        cell.textLabel!.text = file.lastPathComponent
 
         return cell
     }
 
-    /*
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            let url = theFiles![indexPath.row]
+            do {
+                tableView.beginUpdates()
+                try NSFileManager.defaultManager().removeItemAtURL(url)
+                print("Deleted file:", url.lastPathComponent)
+                theFiles?.removeAtIndex(indexPath.row)
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                tableView.endUpdates()
+            } catch let error as NSError {
+                print("Could not delete file:", error)
+                showError("The file \"\(url.lastPathComponent)\" could not be removed.", title: "Error")
+            }
         }
     }
-    */
 
 
 }
