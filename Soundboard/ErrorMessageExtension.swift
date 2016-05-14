1333095069953e45e9ae12685d354b2194f684b6
Jan Hindermann <jan@dfragment.net>
2016-05-14 15:30:48 +0200

    Added extension for displaying simple alert views

---
+++ Soundboard/ErrorMessageExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ErrorMessageExtension.swift
+//  Soundboard
+//
+//  Created by Jan on 14.05.16.
+//  Copyright © 2016 dfragment.net. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    public func showError(message: String!, title: String!) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        self.showViewController(alert, sender: nil)
+    }
+    
+}
